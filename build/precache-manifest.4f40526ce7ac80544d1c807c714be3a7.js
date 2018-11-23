ad30aab16a248823acad03535b06954b9c4a7b28
Armando Longoria <almxtr@outlook.com>
2018-11-22 18:05:54 -0600

    Final version

---
+++ build/precache-manifest.4f40526ce7ac80544d1c807c714be3a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7187cdce27e2a1c84c45",
+    "url": "/static/js/main.7187cdce.chunk.js"
+  },
+  {
+    "revision": "39a0acda35a094cf9c2c",
+    "url": "/static/js/1.39a0acda.chunk.js"
+  },
+  {
+    "revision": "7187cdce27e2a1c84c45",
+    "url": "/static/css/main.fe92cd88.chunk.css"
+  },
+  {
+    "revision": "3f01daee844a1f1663deff4d35b4cf47",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Todos from "./Todos";
+import AddTodo from "./AddTodo";
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: "Comprar leche y pan" },
+      { id: 2, content: "Mandar invitaciones del bautizo" }
+    ]
+  };
+  deleteTodo = id => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos
+    });
+  };
+  addTodo = todo => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  };
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Tareas Pendientes</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+    );
+  }
+}
+export default App;
